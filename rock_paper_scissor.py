752d92290b29abb04189bde8ca418a7111cf95d3
Lowa Ewesit <lowaewesi@gmail.com>
2024-04-02 11:48:47 +0300

    Get a random choice for the user

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,9 +1,21 @@
-## Get and print a random choice for the computer
+# Get and print a random choice for the computer
 import random
 
-def get_computer_choice():
-    return random.choice(['rock', 'paper', 'scissors'])
+# def get_computer_choice():
+#     return random.choice(['rock', 'paper', 'scissors'])
 
-computer_choice = get_computer_choice();
+# computer_choice = get_computer_choice();
 
-print(computer_choice);
+# print(computer_choice);
+
+#Get a random choice for the user
+
+def get_user_choice():
+    user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+    while user_choice not in ['rock', 'paper', 'scissors']:
+        print("Invalid choice. Please enter rock, paper, or scissors.")
+        user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+    return user_choice
+
+user_choice = get_user_choice();
+        
