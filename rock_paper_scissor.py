449eec5d7b62e92c42b50a739308a3e5ace20f01
Lowa Ewesit <lowaewesi@gmail.com>
2024-04-02 12:05:44 +0300

    Determine winner

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,10 +1,10 @@
 # Get and print a random choice for the computer
 import random
 
-# def get_computer_choice():
-#     return random.choice(['rock', 'paper', 'scissors'])
+def get_computer_choice():
+    return random.choice(['rock', 'paper', 'scissors'])
 
-# computer_choice = get_computer_choice();
+computer_choice = get_computer_choice();
 
 # print(computer_choice);
 
@@ -18,4 +18,18 @@ def get_user_choice():
     return user_choice
 
 user_choice = get_user_choice();
+
+#Determine winner
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'paper' and computer_choice == 'rock') or \
+         (user_choice == 'scissors' and computer_choice == 'paper'):
+        return "You win!"
+    else:
+        return "Computer wins!"
         
+winner = determine_winner(user_choice, computer_choice);
+print(winner)
